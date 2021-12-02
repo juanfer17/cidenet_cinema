ef2af939a8c71074ded79dbb4a332a08db4fde9e
unknown <juanferja17@gmail.com>
2021-12-02 10:55:50 -0500

    Fondo blue, y se estiliza tabla

--- src/main/webapp/app/admin/user-management/list/user-management.component.ts
+++ src/main/webapp/app/admin/user-management/list/user-management.component.ts
@@ -14,6 +14,7 @@ import { UserManagementDeleteDialogComponent } from '../delete/user-management-d
 @Component({
   selector: 'jhi-user-mgmt',
   templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.scss'],
 })
 export class UserManagementComponent implements OnInit {
   currentAccount: Account | null = null;
